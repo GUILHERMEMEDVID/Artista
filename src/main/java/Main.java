cf74389cb36c7e882c5ee6f6bcb6fb0af1a77806
GUILHERMEMEDVID <169068246+GUILHERMEMEDVID@users.noreply.github.com>
2024-05-13 15:18:53 +0000

    salvar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
   public static void main(String[] args) { System.out.println("|---------------|");
     System.out.println("|banda: Skindred|");
 System.out.println("|---------------|");
-System.out.println(" ");
-System.out.println("album:babylon(2002-2006)");
-System.out.println("");}
+System.out.println("genêro: Nu metal, reggae metal, rap metal");
+System.out.println("membros:Benji Webbe, Daniel Pugsley, Mikey Demus, Jeff Rose, Martyn Ford");
+ System.out.println("------------------------------------------------------------------------");
+System.out.println("album:babylon(2002-2006)|");
+System.out.println("-------------------------");
+}
 }
