dd7dd32cc0a81c1645c20052643fff2c87e3b2d4
GUILHERMEMEDVID <169068246+GUILHERMEMEDVID@users.noreply.github.com>
2024-05-13 14:58:14 +0000

    dia 13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
   public static void main(String[] args) { System.out.println("|---------------|");
     System.out.println("|banda: Skindred|");
 System.out.println("|---------------|");
-}
+System.out.println(" ");
+System.out.println("album:babylon(2002-2006)");
+System.out.println("");}
 }
